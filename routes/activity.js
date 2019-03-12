eea5e7c924fc4ce9043d4663eff4317825ab7b06
Konstantin Mednikov <irondsd@gmail.com>
2019-03-12 17:44:58 +0300

    error message

--- routes/activity.js
+++ routes/activity.js
@@ -48,7 +48,15 @@ router.post('/:id/activity', (req, res) => {
 
 router.put('/:uid/activity/:aid', (req, res) => {
     if (!validate.activity_record(req)) {
-        return res.status(400).send()
+        return res.status(400).send({
+            error: 'Did not receive enough information'
+            example: {
+                "activity_type": "Walking",
+                "duration": 360,
+                "data": "{ steps: 44, distance: 55.3, successful: true }",
+                "time_started": 1552401333
+            }
+        })
     }
 
     let sql = `update activity set activity_type = '${req.body.activity_type}', time_started = '${req.body.time_started}', duration = '${req.body.duration}', data = '${req.body.data}' where id = ${req.params.aid}`
