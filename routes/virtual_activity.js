455fef85430bcd0808b2a2c3e0c9885170dc4349
Konstantin Mednikov <irondsd@gmail.com>
2019-09-17 14:07:56 +0300

    virtual activity update remodel

--- routes/virtual_activity.js
+++ routes/virtual_activity.js
@@ -9,31 +9,25 @@ let { taskMarkCompleted } = require('../helpers/taskMarkCompleted')
 
 router.get('/:uid/virtual_activity', (req, res) => {
     let timeframe = ``
-    if (req.query.from) {
-        timeframe += ` and time_started > ${req.query.from} `
-    }
-    if (req.query.to) {
-        timeframe += ` and time_started < ${req.query.to} `
-    }
+    if (req.query.from) timeframe += ` and time_started > ${req.query.from} `
+    if (req.query.to) timeframe += ` and time_started < ${req.query.to} `
+
     let deleted = ` and deleted = 0`
-    if (req.query.deleted == 1) {
-        deleted = ` and deleted = 1`
-    } else if (req.query.deleted == 'all') {
-        deleted = ''
-    }
+    if (req.query.deleted == 1) deleted = ` and deleted = 1`
+    else if (req.query.deleted == 'all') deleted = ''
+
     let uploaded = ``
-    if (req.query.uploaded) {
-        uploaded = `, uploaded`
-    }
-    let version = ``
-    if (req.query.version) {
-        version = `, version`
-    }
+    if (req.query.uploaded) uploaded = `, uploaded`
+
+    let doctor_id = ``
+    if (req.body.doctor_id) doctor_id = ` and doctor_id = ${req.body.doctor_id}`
+
     sql =
-        `select activity_id, users_id, doctor_id, activity_type, time_started, time_ended, tasks_id, ref_id, last_updated, data${uploaded}${version} from virtual_activity where users_id = ` +
+        `select activity_id, users_id, doctor_id, activity_type, time_started, time_ended, tasks_id, ref_id, last_updated, data${uploaded} from virtual_activity where users_id = ` +
         req.params.uid +
         timeframe +
-        deleted
+        deleted +
+        doctor_id
 
     console.log(sql)
     db.all(sql, function(err, rows) {
@@ -50,20 +44,16 @@ router.get('/:uid/virtual_activity', (req, res) => {
 
 router.get('/:uid/virtual_activity/:aid', (req, res) => {
     let uploaded = ``
-    if (req.query.uploaded) {
-        uploaded = `, uploaded`
-    }
-    let version = ``
-    if (req.query.version) {
-        version = `, version`
-    }
+    if (req.query.uploaded) uploaded = `, uploaded`
+
     let deleted = ` and deleted = 0`
-    if (req.query.deleted == 1) {
-        deleted = ` and deleted = 1`
-    } else if (req.query.deleted == 'all') {
-        deleted = ''
-    }
-    query = `select activity_id, users_id, doctor_id, activity_type, time_started, time_ended, tasks_id, ref_id, last_updated, data${uploaded}${version} from virtual_activity where activity_id = ${req.params.aid} and users_id = ${req.params.uid}${deleted}`
+    if (req.query.deleted == 1) deleted = ` and deleted = 1`
+    else if (req.query.deleted == 'all') deleted = ''
+
+    let doctor_id = ``
+    if (req.body.doctor_id) doctor_id = `and doctor_id = ${req.body.doctor_id}`
+
+    query = `select activity_id, users_id, doctor_id, activity_type, time_started, time_ended, tasks_id, ref_id, last_updated, data${uploaded} from virtual_activity where activity_id = ${req.params.aid} and users_id = ${req.params.uid}${deleted} ${doctor_id}`
 
     db.all(query, (err, rows) => {
         if (err) {
@@ -76,8 +66,6 @@ router.get('/:uid/virtual_activity/:aid', (req, res) => {
             return res.status(404).send()
         }
     })
-
-    // updateLastSeen(req.params.uid)
 })
 
 router.post('/:uid/virtual_activity', (req, res) => {
@@ -85,24 +73,34 @@ router.post('/:uid/virtual_activity', (req, res) => {
         return res.status(400).send()
     }
 
-    let sql = `insert into virtual_activity(activity_id, users_id, doctor_id, activity_type, time_started, data, tasks_id, time_ended, last_updated, uploaded) values 
-            ('${req.body.id}', '${req.params.uid}', '${req.body.doctor_id}', '${req.body.activity_type}', '${
-        req.body.time_started
-    }', '${JSON.stringify(req.body.data)}', '${req.body.tasks_id ? req.body.tasks_id : null}', '${
-        req.body.time_ended ? req.body.time_ended : null
-    }', '${req.body.last_updated}', '${timestamp()}')`
-
-    // console.log(sql)
-    db.run(sql, function(err, rows) {
+    let check = `select id from virtual_activity where activity_id = '${req.body.id}' and doctor_id = '${req.body.doctor_id}' and deleted = '0'`
+    db.all(check, function(err, rows) {
         if (err) {
-            console.log(err)
-        } else {
-            res.status(201).send({
-                id: this.lastID
+            res.status(500).send({
+                error: err
             })
-            if (req.body.tasks_id && req.body.data.successful) {
-                taskMarkCompleted(req.body.tasks_id)
-            }
+        }
+        if (rows[0]) {
+            return updateVirtualActivity(req, res)
+        } else {
+            let sql = `insert into virtual_activity(activity_id, users_id, doctor_id, activity_type, time_started, data, tasks_id, time_ended, last_updated, uploaded) values
+                           ('${req.body.id}', '${req.params.uid}', '${req.body.doctor_id}', '${
+                req.body.activity_type
+            }', '${req.body.time_started}', '${JSON.stringify(req.body.data)}', '${
+                req.body.tasks_id ? req.body.tasks_id : null
+            }', '${req.body.time_ended ? req.body.time_ended : null}', '${req.body.last_updated}', '${timestamp()}')`
+            db.run(sql, function(err, rows) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res.status(201).send({
+                        id: this.lastID
+                    })
+                    if (req.body.tasks_id && req.body.data.successful) {
+                        taskMarkCompleted(req.body.tasks_id)
+                    }
+                }
+            })
         }
     })
 })
@@ -119,36 +117,44 @@ router.put('/:uid/virtual_activity/:aid', (req, res) => {
         })
     }
 
-    let queryPreserve = `insert into virtual_activity (activity_id, users_id, doctor_id activity_type, time_started, time_ended, tasks_id, ref_id, last_updated, data, deleted, version, uploaded) SELECT activity_id, users_id, doctor_id, activity_type, time_started, time_ended, tasks_id, ref_id, last_updated, data, 1, version, uploaded FROM virtual_activity where activity_id = '${req.params.aid}'`
+    updateVirtualActivity(req, res)
+})
+
+function updateVirtualActivity(req, res) {
+    let id = req.params.aid
+
+    if (req.body.id) id = req.body.id
+
+    let queryPreserve = `insert into virtual_activity (activity_id, users_id, doctor_id,
+         activity_type, time_started, time_ended, tasks_id, ref_id, last_updated, data, deleted, uploaded) SELECT activity_id, users_id, doctor_id, activity_type, time_started, time_ended, tasks_id, ref_id, last_updated, data, 1, uploaded FROM virtual_activity where activity_id = '${id}' and doctor_id = ${req.body.doctor_id}`
     db.run(queryPreserve, (err, rows) => {
         if (err) {
             console.log(err)
         } else {
-            console.log('preserved row')
-        }
-    })
-    let sql = `update virtual_activity set activity_type = '${req.body.activity_type}', time_started = '${
-        req.body.time_started
-    }', time_ended = '${req.body.time_ended}', data = '${JSON.stringify(req.body.data)}', last_updated = '${
-        req.params.last_updated
-    }', ref_id = '${req.params.aid}', uploaded = '${timestamp()}' where activity_id = ${req.params.aid}`
-    console.log(sql)
-    db.run(sql, function(err, rows) {
-        if (err) {
-            res.status(400).send({
-                error: err
-            })
-        } else {
-            db.run(
-                `update virtual_activity set ref_id = '${this.lastID}' where activity_id = ${req.params.aid}`,
-                (err, rows) => {
-                    console.log('added ref id')
+            let sql = `update virtual_activity set activity_type = '${req.body.activity_type}', time_started = '${
+                req.body.time_started
+            }', time_ended = '${req.body.time_ended}', data = '${JSON.stringify(req.body.data)}', last_updated = '${
+                req.params.last_updated
+            }', ref_id = '${id}', uploaded = '${timestamp()}' where activity_id = ${id}`
+            console.log(sql)
+            db.run(sql, function(err, rows) {
+                if (err) {
+                    res.status(400).send({
+                        error: err
+                    })
+                } else {
+                    db.run(
+                        `update virtual_activity set ref_id = '${this.lastID}' where activity_id = ${id}`,
+                        (err, rows) => {
+                            console.log('added ref id')
+                        }
+                    )
+                    res.status(201).send(rows)
                 }
-            )
-            res.status(201).send(rows)
+            })
         }
     })
-})
+}
 
 router.delete('/:uid/virtual_activity/:aid', (req, res) => {
     if (false) {
@@ -158,9 +164,7 @@ router.delete('/:uid/virtual_activity/:aid', (req, res) => {
         })
     }
 
-    let sql = `update virtual_activity set deleted = '1', uploaded = '${timestamp()}' where activity_id = '${
-        req.params.aid
-    }'`
+    let sql = `update virtual_activity set deleted = '1' where activity_id = '${req.params.aid}'`
     db.run(sql, function(err, rows) {
         if (err) {
             res.status(400).send({
