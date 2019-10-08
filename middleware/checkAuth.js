0235d301116beb9c2c76c119238e1339cc9c8129
Konstantin Mednikov <irondsd@gmail.com>
2019-10-08 14:10:00 +0300

    auth by both query api_key and auth header

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -4,13 +4,19 @@ const errors = require('../helpers/errors')
 const log = require('../helpers/logger')
 
 module.exports = (req, res, next) => {
+    let api_key = req.query.api_key
+
+    if (req.headers.authorization)
+        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer')
+            api_key = req.headers.authorization.split(' ')[1]
+
     try {
-        const decoded = jwt.verify(req.query.api_key, process.env.TOKEN_KEY)
+        const decoded = jwt.verify(api_key, process.env.TOKEN_KEY)
         req.decoded = decoded
         updateLastSeen(decoded.id)
         next()
     } catch (error) {
-        log(`unsuccessful api key decode ${req.query.api_key}`)
+        log(`unsuccessful api key decode ${api_key}`)
         errors.unauthorized(res)
     }
 }
