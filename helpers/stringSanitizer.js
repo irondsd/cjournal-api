31ffca9b7bb3dd5b514fd7af40b932c098847a98
Konstantin Mednikov <irondsd@gmail.com>
2019-10-10 13:49:13 +0300

    error proofing string sanitizer

--- helpers/stringSanitizer.js
+++ helpers/stringSanitizer.js
@@ -1,3 +1,6 @@
 module.exports = string => {
+    if (!string) return ''
+    if (typeof string !== 'string' || typeof string !== 'number') return ''
+    if (typeof string === 'number') string + ''
     return string.replace(/[|&;$%@"'<>()+,]/g, '')
 }
