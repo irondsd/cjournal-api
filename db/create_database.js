752c57d26d90ed7be6e29504caa4d874f9a10093
Konstantin Mednikov <irondsd@gmail.com>
2019-12-04 16:10:18 +0300

    changed device_type to idinv

--- db/create_database.js
+++ db/create_database.js
@@ -1,15 +1,15 @@
-const log = require('../helpers/logger')
-const sqlite = require('sqlite3')
-const db = new sqlite.Database('trackers.db')
-let populate = true // add sample values to the db
-let errors = false
+const log = require("../helpers/logger");
+const sqlite = require("sqlite3");
+const db = new sqlite.Database("trackers.db");
+let populate = true; // add sample values to the db
+let errors = false;
 
 db.serialize(() => {
-    db.run(
-        `create table if not exists users (
+  db.run(
+    `create table if not exists users (
                 id integer primary key, 
                 name text, 
-                device_type text, 
+                idinv text, 
                 gender text,
                 birthday text,
                 email text unique,
@@ -20,15 +20,15 @@ db.serialize(() => {
                 permissions integer not null default '1',
                 last_seen datetime)`,
 
-        err => {
-            if (err) {
-                errors = true
-            }
-        }
-    )
+    err => {
+      if (err) {
+        errors = true;
+      }
+    }
+  );
 
-    db.run(
-        `create table if not exists activity (
+  db.run(
+    `create table if not exists activity (
         id integer primary key,
         users_id integer not null,
         activity_type text not null,
@@ -45,44 +45,44 @@ db.serialize(() => {
         foreign key (users_id) references users(id),
         foreign key (tasks_id) references tasks(id)
     )`,
-        err => {
-            if (err) {
-                errors = true
-            }
-        }
-    )
+    err => {
+      if (err) {
+        errors = true;
+      }
+    }
+  );
 
-    db.run(
-        `create table if not exists prescriptions (
+  db.run(
+    `create table if not exists prescriptions (
         users_id integer not null primary key,
         course_therapy text,
         relief_of_attack text,
         tests text,
         foreign key (users_id) references users(id)
     )`,
-        err => {
-            if (err) {
-                errors = true
-            }
-        }
-    )
+    err => {
+      if (err) {
+        errors = true;
+      }
+    }
+  );
 
-    db.run(
-        `create table if not exists doctor (
+  db.run(
+    `create table if not exists doctor (
         patient_id integer not null,
         doctor_id integer not null,
         foreign key (patient_id) references users(id)
         foreign key (doctor_id) references users(id)
     )`,
-        err => {
-            if (err) {
-                errors = true
-            }
-        }
-    )
+    err => {
+      if (err) {
+        errors = true;
+      }
+    }
+  );
 
-    db.run(
-        `create table if not exists tasks (
+  db.run(
+    `create table if not exists tasks (
         id integer primary key,
         users_id integer not null,
         activity_type text not null,
@@ -95,15 +95,15 @@ db.serialize(() => {
         deleted bool default false,
         foreign key (users_id) references users(id)
     )`,
-        err => {
-            if (err) {
-                errors = true
-            }
-        }
-    )
+    err => {
+      if (err) {
+        errors = true;
+      }
+    }
+  );
 
-    db.run(
-        `create table if not exists virtual_activity (
+  db.run(
+    `create table if not exists virtual_activity (
         id integer primary key,
         activity_id integer,
         users_id integer not null,
@@ -124,157 +124,157 @@ db.serialize(() => {
         foreign key (tasks_id) references tasks(id),
         foreign key (activity_id) references activity(id)
     )`,
-        err => {
-            if (err) {
-                errors = true
-            }
-        }
-    )
-})
+    err => {
+      if (err) {
+        errors = true;
+      }
+    }
+  );
+});
 
 if (populate) {
-    db.run(
-        `INSERT INTO users(name, device_type, birthday, gender, last_seen, email, password, information, language) VALUES ('Alexander Feldman', 'Shovel','10.05.1957', 'male', '1550507313', 'ggn00b@mail.ru', '$2a$10$teACha.MBCW68XIqYHAZielRJa5qSbSx6DKf4ihAqTVqOgJtg3aoe', 'You are the patint of Whatever hostpital. Your doctor is Donald Trump. Wut? You crazy or what? You can contact him on the phone number +13947576392', 'ru')`,
-        err => {
-            if (err) {
-                errors = true
-            }
-        }
-    )
-    db.run(
-        `INSERT INTO users(name, device_type, birthday, gender, last_seen, email, password, information, language) VALUES ('Carl Sagan', 'Telescope', '10.05.1987', 'male','1550507313', 'ggn00b@mail.ua', '$2a$10$teACha.MBCW68XIqYHAZielRJa5qSbSx6DKf4ihAqTVqOgJtg3aoe', 'You are the patint of Whatever hostpital. Your doctor is Donald Trump. Wut? You crazy or what? You can contact him on the phone number +13947576392', 'es')`,
-        err => {
-            if (err) {
-                errors = true
-            }
-        }
-    )
-    db.run(
-        `INSERT INTO users(name, device_type, birthday, gender, last_seen, email, password, information, language, permissions) VALUES ('Max Plank', 'Microscope', '10.05.1963', 'male','1550507313', 'ggn000b@gmail.com', '$2a$10$teACha.MBCW68XIqYHAZielRJa5qSbSx6DKf4ihAqTVqOgJtg3aoe', 'You are the patint of Whatever hostpital. Your doctor is Donald Trump. Wut? You crazy or what? You can contact him on the phone number +13947576392', 'en', '3')`,
-        err => {
-            if (err) {
-                errors = true
-            }
-        }
-    )
-
-    let data = {
-        steps: 10.3,
-        distance: 15,
-        sucessfull: true
+  db.run(
+    `INSERT INTO users(name, idinv, birthday, gender, last_seen, email, password, information, language) VALUES ('Alexander Feldman', '111','10.05.1957', 'male', '1550507313', 'ggn00b@mail.ru', '$2a$10$teACha.MBCW68XIqYHAZielRJa5qSbSx6DKf4ihAqTVqOgJtg3aoe', 'You are the patint of Whatever hostpital. Your doctor is Donald Trump. Wut? You crazy or what? You can contact him on the phone number +13947576392', 'ru')`,
+    err => {
+      if (err) {
+        errors = true;
+      }
     }
-
-    db.run(
-        `insert into activity(users_id, activity_type, time_started, last_updated, data) values ('1', 'Walking', '1554197138', '1554195281', json('${JSON.stringify(
-            data
-        )}'))`,
-        err => {
-            if (err) {
-                errors = true
-            }
-        }
-    )
-
-    db.run(
-        `insert into activity(users_id, activity_type, time_started, last_updated, data) values ('1', 'Walking', '1554197138', '1554195281', json('${JSON.stringify(
-            data
-        )}'))`,
-        err => {
-            if (err) {
-                log(err)
-                errors = true
-            }
-        }
-    )
-
-    db.run(
-        `insert into activity(users_id, activity_type, time_started, last_updated, data) values ('3', 'Walking', '1554197138', '1554195281', json('${JSON.stringify(
-            data
-        )}'))`,
-        err => {
-            if (err) {
-                log(err)
-                errors = true
-            }
-        }
-    )
-
-    db.run(
-        `insert into tasks(users_id, activity_type, time, last_updated) values ('1', 'Walking', 1555166888, 1555241651)`,
-        err => {
-            if (err) {
-                log(err)
-                errors = true
-            }
-        }
-    )
-
-    db.run(
-        `insert into tasks(users_id, activity_type, time, last_updated) values ('1', 'Walking', 1555166888, 1555241651)`,
-        err => {
-            if (err) {
-                log(err)
-                errors = true
-            }
-        }
-    )
-
-    db.run(
-        `insert into tasks(users_id, activity_type, time, last_updated) values ('3', 'Walking', 1555166888, 1555241651)`,
-        err => {
-            if (err) {
-                log(err)
-                errors = true
-            }
-        }
-    )
-
-    db.run(
-        `insert into prescriptions(users_id, course_therapy, relief_of_attack, tests) values ('1', '["Acebutolol", "Atenolol"]', '["Bisoprolol", "Betaxolol"]', '["Nadolol", "Propranolol"]')`,
-        err => {
-            if (err) {
-                log(err)
-                errors = true
-            }
-        }
-    )
-
-    db.run(
-        `insert into prescriptions(users_id, course_therapy, relief_of_attack, tests) values ('2', '["Acebutolol", "Atenolol"]', '["Bisoprolol", "Betaxolol"]', '["Nadolol", "Propranolol"]')`,
-        err => {
-            if (err) {
-                log(err)
-                errors = true
-            }
-        }
-    )
-
-    db.run(
-        `insert into prescriptions(users_id, course_therapy, relief_of_attack, tests) values ('3', '["Acebutolol", "Atenolol"]', '["Bisoprolol", "Betaxolol"]', '["Nadolol", "Propranolol"]')`,
-        err => {
-            if (err) {
-                log(err)
-                errors = true
-            }
-        }
-    )
-
-    if (errors === false) {
-        //let's now make sure the records are there
-        db.each(`SELECT * FROM users`, (err, records) => {
-            console.log(records)
-        })
-
-        db.each(`select * from activity`, (err, records) => {
-            console.log(records)
-        })
-
-        db.each(`select * from tasks`, (err, records) => {
-            console.log(records)
-        })
-
-        log(`Sucessfully created the database`)
+  );
+  db.run(
+    `INSERT INTO users(name, idinv, birthday, gender, last_seen, email, password, information, language) VALUES ('Carl Sagan', '222', '10.05.1987', 'male','1550507313', 'ggn00b@mail.ua', '$2a$10$teACha.MBCW68XIqYHAZielRJa5qSbSx6DKf4ihAqTVqOgJtg3aoe', 'You are the patint of Whatever hostpital. Your doctor is Donald Trump. Wut? You crazy or what? You can contact him on the phone number +13947576392', 'es')`,
+    err => {
+      if (err) {
+        errors = true;
+      }
     }
+  );
+  db.run(
+    `INSERT INTO users(name, idinv, birthday, gender, last_seen, email, password, information, language, permissions) VALUES ('Max Plank', '333', '10.05.1963', 'male','1550507313', 'ggn000b@gmail.com', '$2a$10$teACha.MBCW68XIqYHAZielRJa5qSbSx6DKf4ihAqTVqOgJtg3aoe', 'You are the patint of Whatever hostpital. Your doctor is Donald Trump. Wut? You crazy or what? You can contact him on the phone number +13947576392', 'en', '3')`,
+    err => {
+      if (err) {
+        errors = true;
+      }
+    }
+  );
+
+  let data = {
+    steps: 10.3,
+    distance: 15,
+    sucessfull: true
+  };
+
+  db.run(
+    `insert into activity(users_id, activity_type, time_started, last_updated, data) values ('1', 'Walking', '1554197138', '1554195281', json('${JSON.stringify(
+      data
+    )}'))`,
+    err => {
+      if (err) {
+        errors = true;
+      }
+    }
+  );
+
+  db.run(
+    `insert into activity(users_id, activity_type, time_started, last_updated, data) values ('1', 'Walking', '1554197138', '1554195281', json('${JSON.stringify(
+      data
+    )}'))`,
+    err => {
+      if (err) {
+        log(err);
+        errors = true;
+      }
+    }
+  );
+
+  db.run(
+    `insert into activity(users_id, activity_type, time_started, last_updated, data) values ('3', 'Walking', '1554197138', '1554195281', json('${JSON.stringify(
+      data
+    )}'))`,
+    err => {
+      if (err) {
+        log(err);
+        errors = true;
+      }
+    }
+  );
+
+  db.run(
+    `insert into tasks(users_id, activity_type, time, last_updated) values ('1', 'Walking', 1555166888, 1555241651)`,
+    err => {
+      if (err) {
+        log(err);
+        errors = true;
+      }
+    }
+  );
+
+  db.run(
+    `insert into tasks(users_id, activity_type, time, last_updated) values ('1', 'Walking', 1555166888, 1555241651)`,
+    err => {
+      if (err) {
+        log(err);
+        errors = true;
+      }
+    }
+  );
+
+  db.run(
+    `insert into tasks(users_id, activity_type, time, last_updated) values ('3', 'Walking', 1555166888, 1555241651)`,
+    err => {
+      if (err) {
+        log(err);
+        errors = true;
+      }
+    }
+  );
+
+  db.run(
+    `insert into prescriptions(users_id, course_therapy, relief_of_attack, tests) values ('1', '["Acebutolol", "Atenolol"]', '["Bisoprolol", "Betaxolol"]', '["Nadolol", "Propranolol"]')`,
+    err => {
+      if (err) {
+        log(err);
+        errors = true;
+      }
+    }
+  );
+
+  db.run(
+    `insert into prescriptions(users_id, course_therapy, relief_of_attack, tests) values ('2', '["Acebutolol", "Atenolol"]', '["Bisoprolol", "Betaxolol"]', '["Nadolol", "Propranolol"]')`,
+    err => {
+      if (err) {
+        log(err);
+        errors = true;
+      }
+    }
+  );
+
+  db.run(
+    `insert into prescriptions(users_id, course_therapy, relief_of_attack, tests) values ('3', '["Acebutolol", "Atenolol"]', '["Bisoprolol", "Betaxolol"]', '["Nadolol", "Propranolol"]')`,
+    err => {
+      if (err) {
+        log(err);
+        errors = true;
+      }
+    }
+  );
+
+  if (errors === false) {
+    //let's now make sure the records are there
+    db.each(`SELECT * FROM users`, (err, records) => {
+      console.log(records);
+    });
+
+    db.each(`select * from activity`, (err, records) => {
+      console.log(records);
+    });
+
+    db.each(`select * from tasks`, (err, records) => {
+      console.log(records);
+    });
+
+    log(`Sucessfully created the database`);
+  }
 }
 
-db.close()
+db.close();
