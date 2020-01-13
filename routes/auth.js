6331a062dd11ba704b8022b8bb9c0afabdff6e42
Konstantin Mednikov <irondsd@gmail.com>
2020-01-13 14:26:50 +0300

    login route

--- routes/auth.js
+++ routes/auth.js
@@ -5,65 +5,30 @@ const db = new sqlite.Database('./db/trackers.db')
 const errors = require('../helpers/errors')
 const log = require('../helpers/logger')
 const fetch = require('node-fetch')
+const checkAuth = require('../middleware/checkAuth')
+const objectify = require('../helpers/objectify')
 require('dotenv').config()
-var FormData = require('form-data')
-const createUser = require('../helpers/createUser')
-const updateTokens = require('../helpers/updateTokens')
 
-router.post('/auth', (req, res, next) => {
-    const host = process.env.IDENTITY_HOST
-    const port = process.env.IDENTITY_PORT
-    const url = `http://${host.trim()}:${port.trim()}/connect/token`
-
-    var formData = new FormData()
-
-    formData.append('grant_type', 'password')
-    formData.append('client_id', 'ApiClient')
-    formData.append('password', req.body.password)
-    formData.append('username', req.body.username)
-
-    var request = {
-        method: 'POST',
-        headers: {
-            Accept: 'multipart/form-data',
-        },
-        body: formData,
-    }
-
-    fetch(url, request)
-        .then(response => {
-            if (response.status === 200) {
-                response.json().then(response => {
-                    fetch(`http://${host.trim()}:${port.trim()}/connect/userinfo`, {
-                        method: 'POST',
-                        headers: {
-                            Authorization: 'Bearer ' + response.access_token,
-                        },
-                    })
-                        .then(response => response.json())
-                        .then(new_res => {
-                            createUser(new_res.name, response.access_token, response.refresh_token)
-                                .then(() => {
-                                    log.info(`user ${new_res.name} successfully created`)
-                                    updateTokens(
-                                        new_res.name,
-                                        response.access_token,
-                                        response.refresh_token,
-                                    ).then()
-                                })
-                                .catch(err => {
-                                    log.error(err)
-                                })
-                            res.send(new_res.name)
-                        })
-                })
-            } else {
-                throw new Error()
-            }
-        })
-        .catch(err => {
-            res.status(400).send('Wrong username or password')
-        })
+router.post('/login', checkAuth, (req, res, next) => {
+    let query =
+        `select * from users inner join 
+            prescriptions on users.id = prescriptions.users_id
+            where users.id = ` + req.user.id
+    log.debug(query)
+    db.all(query, (err, rows) => {
+        if (err) {
+            log.error(`users internal error ${err}`)
+            return errors.internalError(res)
+        }
+        if (rows.length > 0) {
+            objectify.all(rows)
+            log.info(`user ${req.user.id} | ${req.user.username} has logged in`)
+            return res.send(rows[0])
+        } else {
+            log.info(`get users id not found ${req.user.id}`)
+            return errors.notFound(res)
+        }
+    })
 })
 
 module.exports = router
