df2d5610755934d00e686353ee6b56f4fdb103ac
Konstantin Mednikov <irondsd@gmail.com>
2020-01-31 15:17:07 +0300

    comment for release

--- config/oauth.js
+++ config/oauth.js
@@ -6,11 +6,6 @@ var OidcStrategy = require('passport-openidconnect').Strategy
 passport.use(
     new OidcStrategy(
         {
-            // authorizationURL: 'http://192.168.4.140:7050/connect/authorize',
-            // tokenURL: 'http://192.168.4.140:7050/connect/token',
-            // clientID: 'cjournal-web',
-            // clientSecret: 'i3m0c78ko9cojdqjq706e5u4',
-            // callbackURL: 'http://localhost:3333/api/oauth/callback',
             issuer: `http://217.197.236.242:7050`,
             authorizationURL: `http://217.197.236.242:7050/connect/authorize`,
             tokenURL: `http://217.197.236.242:7050/connect/token`,
