7c6dc04f8d08e15e4c26eda65a0abcbf8eaade91
Konstantin V. Mednikov <mkv@incart.ru>
2020-05-15 00:08:33 +0300

    virtual update

--- middleware/validateVirtual.js
+++ middleware/validateVirtual.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
     if (req.body.id && req.body.activity_type && req.body.time_started && req.body.doctor_id) {
         next()
     } else {
-        log.info(`activity is not validated ${JSON.stringify(req.body)}`)
+        log.info(`virtual activity is not validated ${JSON.stringify(req.body)}`)
         errors.incompleteInput(res)
     }
 }
