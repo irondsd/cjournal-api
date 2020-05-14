7aba0b0165f4321bb597d89b6bcdede88fe2dc2b
Konstantin Mednikov <irondsd@gmail.com>
2020-05-14 23:01:57 +0300

    mistake fix

--- middleware/validateVirtual.js
+++ middleware/validateVirtual.js
@@ -2,7 +2,7 @@ const errors = require('../helpers/errors')
 const log = require('../helpers/logger')
 
 module.exports = (req, res, next) => {
-    if ((req.body.id, req.body.activity_type && req.body.time_started && req.body.doctor_id)) {
+    if (req.body.id && req.body.activity_type && req.body.time_started && req.body.doctor_id) {
         next()
     } else {
         log.info(`activity is not validated ${JSON.stringify(req.body)}`)
