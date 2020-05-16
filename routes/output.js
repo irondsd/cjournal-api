9f218230f25de5b54d9820dd883934efeedfc9c8
Konstantin Mednikov <irondsd@gmail.com>
2020-05-16 13:52:46 +0300

    output acn made

--- routes/output.js
+++ routes/output.js
@@ -2,30 +2,58 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 const errors = require('../helpers/errors')
-const { spawn } = require('child_process')
+const { exec } = require('child_process')
+const JSZip = require('jszip')
+const zip = new JSZip()
+FileSaver = require('file-saver')
 
 router.post('/acn', (req, res) => {
     if (!req.body.id) return errors.incompleteInput(res)
+    exec(
+        `python3 cjconverter/run.py -id ${req.body.id} -t ${req.body.token}`,
+        (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`)
+                return
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`)
+                return
+            }
+            // res.send(stdout)
+            if (stdout.includes('finished')) {
+                // res.send('success')
+                // sendFile(, res)
+                console.log(stdout)
 
-    var dataToSend
-    // spawn new child process to call the python script
-    const python = spawn('python', [
-        'cjconverter/run.py',
-        `-id ${req.body.id}`,
-        `-u 217.197.236.243:8626`,
-        `-t ${123}`,
-    ])
-    // collect data from script
-    python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...')
-        dataToSend = data.toString()
-    })
-    // in close event we are sure that stream from child process is closed
-    python.on('close', code => {
-        console.log(`child process close all stdio with code ${code}`)
-        // send data to browser
-        res.send(dataToSend)
-    })
+                let re = /generated file: (.+acn)/gm
+                let filename = re.exec(stdout)[1]
+
+                if (filename) sendFile(filename, res)
+                else res.status(400).send()
+            }
+        },
+    )
 })
+
+const sendFile = (filename, res) => {
+    const filePath = './output/' + filename // or any file format
+
+    // Check if file specified by the filePath exists
+    fs.exists(filePath, function (exists) {
+        if (exists) {
+            // Content-type is very interesting part that guarantee that
+            // Web browser will handle response in an appropriate manner.
+            res.writeHead(200, {
+                'Content-Type': 'application/octet-stream',
+                'Content-Disposition': 'attachment; filename=' + filename,
+            })
+            fs.createReadStream(filePath).pipe(res)
+        } else {
+            res.writeHead(400, { 'Content-Type': 'text/plain' })
+            res.end('ERROR File does not exist')
+        }
+    })
+}
 
 module.exports = router
