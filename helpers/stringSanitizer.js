9d8e9a2efd8164bd27a1249a3db5317af3ab2cec
Konstantin Mednikov <irondsd@gmail.com>
2020-05-26 23:06:16 +0300

    string sanitizer bug fix

--- helpers/stringSanitizer.js
+++ helpers/stringSanitizer.js
@@ -1,6 +1,6 @@
 module.exports = string => {
     if (!string) return ''
     if (typeof string !== 'string' && typeof string !== 'number') return ''
-    if (typeof string === 'number') string + ''
+    if (typeof string === 'number') return string + ''
     return string.replace(/[|&;$%@"'<>()+,]/g, '')
 }
