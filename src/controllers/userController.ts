8713b98a48f82af7e72f49b2bb3194dc7539a431
Konstantin Mednikov <irondsd@gmail.com>
2021-02-16 00:25:05 +0300

    extracted user to user controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from 'express'
+import { User } from '../models/user'
+import stringSanitizer from '../helpers/sanitizeString'
+import * as Errors from '../helpers/errors'
+import Logger from 'helpers/logger'
+
+export const userGetAll = async (req: Request, res: Response) => {
+    const users = await User.find()
+    res.send(users)
+}
+
+export const userGetById = async (req: Request, res: Response) => {
+    const id = stringSanitizer(req.params.id)
+    User.findById(id)
+        .populate('prescriptions')
+        .then((user: typeof User) => {
+            if (!user) return Errors.notFound(res)
+            res.send(user)
+        })
+        .catch((err: any) => {
+            Errors.incorrectInput(res, err.reason.message)
+        })
+}
+
+export const userEdit = async (req: Request, res: Response) => {
+    const id = stringSanitizer(req.params.id)
+
+    try {
+        const user = await User.findByIdAndUpdate(id, { ...req.body }, { new: true })
+        res.status(201).send(user)
+    } catch (err) {
+        Logger.error(err.message)
+        Errors.incorrectInput(res)
+    }
+}
+
+export const userLogin = (req: Request, res: Response) => {
+    const user = (req as any).user
+
+    User.find({ sub: user.sub })
+        .then((user: typeof User) => {
+            if (!user) return Errors.notFound(res)
+            res.send(user)
+        })
+        .catch((err: any) => {
+            Errors.incorrectInput(res, err.reason.message)
+        })
+}
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,58 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { User } from '../models/user'
-import stringSanitizer from '../helpers/sanitizeString'
-import * as Errors from '../helpers/errors'
 import { checkAuth } from '../middleware/checkAuth'
+import { userEdit, userGetAll, userGetById, userLogin } from 'controllers/userController'
 
-// Get all users
-router.get('/users/', checkAuth, async (req, res) => {
-    const users = await User.find()
-    res.send(users)
-})
-
-// Get information about the user with specific id
-router.get('/users/:id', checkAuth, async (req, res) => {
-    const id = stringSanitizer(req.params.id)
-    User.findById(id)
-        .populate('prescriptions')
-        .then((user: typeof User) => {
-            if (!user) return Errors.notFound(res)
-            res.send(user)
-        })
-        .catch((err: any) => {
-            Errors.incorrectInput(res, err.reason.message)
-        })
-})
-
-// Update user
-router.put('/users/:uid', async (req, res) => {
-    // TODO:
-    res.send('good')
-})
-
-// should not exist
-router.post('/users/', async (req, res) => {
-    const { username, sub } = req.body
-
-    if (!username || !sub) return res.status(400).send()
-
-    const user = new User({ username, sub })
-    await user.save()
-    res.status(201).send(user)
-})
-
-router.post('/login', checkAuth, (req: any, res) => {
-    const user = req.user
-
-    User.find({ sub: user.sub })
-        .then((user: typeof User) => {
-            if (!user) return Errors.notFound(res)
-            res.send(user)
-        })
-        .catch((err: any) => {
-            Errors.incorrectInput(res, err.reason.message)
-        })
-})
+router.get('/users/', checkAuth, userGetAll)
+router.get('/users/:id', checkAuth, userGetById)
+router.put('/users/:id', checkAuth, userEdit)
+router.post('/login', checkAuth, userLogin)
 
 export { router as usersRouter }
