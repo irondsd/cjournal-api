35e3034e463910d6229f63f1c63f8adb6acdd3e6
Konstantin Mednikov <irondsd@gmail.com>
2021-02-28 16:41:03 +0300

    build fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import defaults from './default'
-var config = require('./' + (process.env.NODE_ENV || 'production') + '.ts')
+var config = require('./' + (process.env.NODE_ENV || 'production'))
 
 export default { ...defaults, ...config }
