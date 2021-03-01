1d56c105d065b894f1206aaba36917e99fd1a213
Konstantin Mednikov <irondsd@gmail.com>
2021-03-01 22:24:27 +0300

    activity by patient

--- src/routes/activity.ts
+++ src/routes/activity.ts
@@ -14,12 +14,20 @@ import {
     activityHistoryGetOne,
 } from '../controllers/activityController'
 
+// get by user
+
 router.get('/users/:uid/activity', async (req, res) => {
     if (!verifyObjectId(req.params.uid)) return Errors.incorrectInput(res)
 
     activityGetMany({ user: req.params.uid })
         .then(activity => res.send(activity))
         .catch(err => Errors.internalError(res, err))
+})
+
+router.get('/users/:uid/activity/:aid', async (req, res) => {
+    activityGetOne({ user: req.params.uid, _id: req.params.aid })
+        .then(activity => res.send(activity))
+        .catch(err => Errors.internalError(res))
 })
 
 router.get('/users/:uid/activity/history', async (req, res) => {
@@ -37,6 +45,40 @@ router.get('/users/:uid/activity/history/:aid', async (req, res) => {
         .then(activity => res.send(activity))
         .catch(err => Errors.internalError(res, err))
 })
+
+// get by patient
+
+router.get('/patients/:pid/activity', async (req, res) => {
+    activityGetMany({ patient: req.params.pid })
+        .then(activity => res.send(activity))
+        .catch(err => Errors.internalError(res, err))
+})
+
+router.get('/patients/:pid/activity/:aid', async (req, res) => {
+    if (!verifyObjectId(req.params.aid)) return Errors.incorrectInput(res)
+
+    activityGetMany({ patient: req.params.pid, _id: req.params.aid })
+        .then(activity => res.send(activity))
+        .catch(err => Errors.internalError(res, err))
+})
+
+router.get('/patients/:pid/activity/history', async (req, res) => {
+    if (!verifyObjectId(req.params.pid)) return Errors.incorrectInput(res)
+
+    activityHistoryGetMany({ patient: req.params.pid })
+        .then(activity => res.send(activity))
+        .catch(err => Errors.internalError(res, err))
+})
+
+router.get('/patients/:uid/activity/history/:aid', async (req, res) => {
+    if (!verifyObjectId(req.params.pid)) return Errors.incorrectInput(res)
+
+    activityHistoryGetOne({ patient: req.params.pid, original: req.params.aid })
+        .then(activity => res.send(activity))
+        .catch(err => Errors.internalError(res, err))
+})
+
+// get by idinv
 
 router.get('/idinv/:idinv/activity', async (req, res) => {
     activityGetMany({ idinv: req.params.idinv })
@@ -58,17 +100,13 @@ router.get('/idinv/:idinv/activity/history/:aid', async (req, res) => {
         .catch(err => Errors.internalError(res, err))
 })
 
-router.get('/users/:uid/activity/:aid', async (req, res) => {
-    activityGetOne({ user: req.params.uid, _id: req.params.aid })
-        .then(activity => res.send(activity))
-        .catch(err => Errors.internalError(res))
-})
-
 router.get('/idinv/:idinv/activity/:aid', async (req, res) => {
     activityGetOne({ idinv: req.params.idinv, _id: req.params.aid })
         .then(activity => res.send(activity))
         .catch(err => Errors.internalError(err))
 })
+
+// post
 
 router.post('/users/:uid/activity', saveFiles, validateActivity, (req, res) => {
     activityCreate({ ...req.body, user: req.params.uid })
@@ -88,6 +126,17 @@ router.post('/idinv/:idinv/activity', saveFiles, validateActivity, (req, res) =>
         })
 })
 
+router.post('/patients/:pid/activity', saveFiles, validateActivity, (req, res) => {
+    activityCreate({ ...req.body, patient: req.params.pid })
+        .then(activity => res.send(activity))
+        .catch(err => {
+            if (err.code === 11000) return res.status(208).send()
+            Errors.internalError(res, err.message)
+        })
+})
+
+// put
+
 router.put('/users/:uid/activity/:aid', saveFiles, validateActivity, (req, res) => {
     activityEdit(req.params.aid, { ...req.body, user: req.params.uid })
         .then(activity => res.send(activity))
@@ -105,6 +154,16 @@ router.put('/idinv/:idinv/activity/:aid', saveFiles, validateActivity, async (re
         })
 })
 
+router.put('/patient/:pid/activity/:aid', saveFiles, validateActivity, async (req, res) => {
+    activityEdit(req.params.aid, { ...req.body, patient: req.params.pid })
+        .then(activity => res.send(activity))
+        .catch(err => {
+            Errors.internalError(res, err.message)
+        })
+})
+
+// delete
+
 router.delete('/users/:uid/activity/:aid', async (req, res) => {
     activityDelete(req.params.aid)
         .then(() => res.status(204).send())
@@ -112,6 +171,12 @@ router.delete('/users/:uid/activity/:aid', async (req, res) => {
 })
 
 router.delete('/idinv/:idinv/activity/:aid', async (req, res) => {
+    activityDelete(req.params.aid)
+        .then(() => res.status(204).send())
+        .catch(err => Errors.internalError(res, err))
+})
+
+router.delete('/patients/:pid/activity/:aid', async (req, res) => {
     activityDelete(req.params.aid)
         .then(() => res.status(204).send())
         .catch(err => Errors.internalError(res, err))
