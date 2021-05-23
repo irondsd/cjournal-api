b8fed9c4b824fbe0992f97a560b62eba730fb4c2
Konstantin Mednikov <irondsd@gmail.com>
2021-05-23 11:03:53 +0300

    validate update

--- src/middleware/validateActivity.ts
+++ src/middleware/validateActivity.ts
@@ -15,3 +15,18 @@ export const validateActivity = (req: Request, res: Response, next: NextFunction
         Errors.incompleteInput(res)
     }
 }
+
+export const validateActivityUpdate = (req: Request, res: Response, next: NextFunction) => {
+    if (
+        (req.body.user || req.body.idinv || req.body.patient) &&
+        req.body.updated_by &&
+        req.body._id &&
+        req.body.activity_type &&
+        req.body.time_started
+    ) {
+        next()
+    } else {
+        Logger.info(`activity update validation error: ${JSON.stringify(req.body)}`)
+        Errors.incompleteInput(res)
+    }
+}
